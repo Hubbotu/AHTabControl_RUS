465d8c702b9b5e4d0d068ac1a99cda545b40f683
Hubbotu <34053496+Hubbotu@users.noreply.github.com>
2020-05-19 07:38:10 +0300

    Add files via upload

---
+++ ruRU.lua
@@ -0,0 +1,9 @@
+AH_TAB_CONTROL_LOCALES.ruRU = function()
+  local L = {}
+
+  L["CURRENTLY_HIDDEN"] = "В настоящее время скрытые вкладки: %s"
+  L["RESTORE"] = "Вкладка восстановлена %s"
+  L["HIDDEN"] = "Вкладка скрыта %s"
+
+  return L
+end
